99b5b43f35ef7c724a087277aa81d5d2c8583adf
Tom Myers <106530686+tommyers-elastic@users.noreply.github.com>
2023-03-06 11:11:33 +0000

    Split unit and int tests (#51)
    * restructure assets_aws package so each assetsinput is a subpackage
    
    * move integration tests to their own package
    
    * rename assets_aws.go -> aws.go
    
    * integration tests -> e2e tests

--- input/assets/aws/aws_test.go
+++ input/assets/aws/aws_test.go
@@ -18,7 +18,6 @@ package aws
 
 import (
 	"context"
-	"os"
 	"testing"
 	"time"
 
@@ -27,58 +26,6 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/stretchr/testify/assert"
 )
-
-//
-//func TestAssetAWS_getConfigForRegion_GivenExplicitCredsInConfig_CreatesCorrectAWSConfig(t *testing.T) {
-//	ctx := context.Background()
-//	inputCfg := config{
-//		BaseConfig: assets.BaseConfig{
-//			Period:     time.Second * 600,
-//			AssetTypes: []string{},
-//		},
-//		Regions:         []string{"eu-west-2", "eu-west-1"},
-//		AccessKeyId:     "accesskey123",
-//		SecretAccessKey: "secretkey123",
-//		SessionToken:    "token123",
-//	}
-//	region := "eu-west-2"
-//	awsCfg, err := getAWSConfigForRegion(ctx, inputCfg, region)
-//	assert.NoError(t, err)
-//	retrievedAWSCreds, err := awsCfg.Credentials.Retrieve(context.Background())
-//	assert.NoError(t, err)
-//
-//	assert.Equal(t, inputCfg.AccessKeyId, retrievedAWSCreds.AccessKeyID)
-//	assert.Equal(t, inputCfg.SecretAccessKey, retrievedAWSCreds.SecretAccessKey)
-//	assert.Equal(t, inputCfg.SessionToken, retrievedAWSCreds.SessionToken)
-//	assert.Equal(t, region, awsCfg.Region)
-//}
-//
-//func TestAssetAWS_getConfigForRegion_GivenLocalCreds_CreatesCorrectAWSConfig(t *testing.T) {
-//	ctx := context.Background()
-//	accessKey := "EXAMPLE_ACCESS_KEY"
-//	secretKey := "EXAMPLE_SECRETE_KEY"
-//	os.Setenv("AWS_ACCESS_KEY", accessKey)
-//	os.Setenv("AWS_SECRET_ACCESS_KEY", secretKey)
-//	inputCfg := config{
-//		BaseConfig: assets.BaseConfig{
-//			Period:     time.Second * 600,
-//			AssetTypes: []string{},
-//		},
-//		Regions:         []string{"eu-west-2", "eu-west-1"},
-//		AccessKeyId:     "",
-//		SecretAccessKey: "",
-//		SessionToken:    "",
-//	}
-//	region := "eu-west-2"
-//	awsCfg, err := getAWSConfigForRegion(ctx, inputCfg, region)
-//	assert.NoError(t, err)
-//	retrievedAWSCreds, err := awsCfg.Credentials.Retrieve(context.Background())
-//	assert.NoError(t, err)
-//
-//	assert.Equal(t, accessKey, retrievedAWSCreds.AccessKeyID)
-//	assert.Equal(t, secretKey, retrievedAWSCreds.SecretAccessKey)
-//	assert.Equal(t, region, awsCfg.Region)
-//)
 
 func TestGetAWSConfigForRegion(t *testing.T) {
 	for _, tt := range []struct {
