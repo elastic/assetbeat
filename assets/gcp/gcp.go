744c2088ce95e18ba15652153c2f2ed222b8a4fb
dmathieu <damien.mathieu@elastic.co>
2023-03-06 09:30:10 +0100

    bootstrap gcp asset

---
+++ assets/gcp/gcp.go
@@ -0,0 +1,107 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package gcp
+
+import (
+	"context"
+	"time"
+
+	"github.com/elastic/go-concert/ctxtool"
+	input "github.com/elastic/inputrunner/input/v2"
+	stateless "github.com/elastic/inputrunner/input/v2/input-stateless"
+
+	"github.com/elastic/beats/v7/libbeat/feature"
+	conf "github.com/elastic/elastic-agent-libs/config"
+)
+
+func Plugin() input.Plugin {
+	return input.Plugin{
+		Name:       "assets_gcp",
+		Stability:  feature.Stable,
+		Deprecated: false,
+		Info:       "assets_gcp",
+		Manager:    stateless.NewInputManager(configure),
+	}
+}
+
+func configure(cfg *conf.C) (stateless.Input, error) {
+	config := defaultConfig()
+	if err := cfg.Unpack(&config); err != nil {
+		return nil, err
+	}
+
+	return newAssetsGCP(config)
+}
+
+func newAssetsGCP(config config) (*assetsGCP, error) {
+	return &assetsGCP{config}, nil
+}
+
+type Config struct {
+	Period time.Duration `config:"period"`
+}
+
+func defaultConfig() config {
+	return config{
+		Config: Config{
+			Period: time.Second * 600,
+		},
+	}
+}
+
+type assetsGCP struct {
+	config
+}
+
+type config struct {
+	Config `config:",inline"`
+}
+
+func (s *assetsGCP) Name() string { return "assets_gcp" }
+
+func (s *assetsGCP) Test(_ input.TestContext) error {
+	return nil
+}
+
+func (s *assetsGCP) Run(inputCtx input.Context, publisher stateless.Publisher) error {
+	ctx := ctxtool.FromCanceller(inputCtx.Cancelation)
+	log := inputCtx.Logger.With("assets_gcp")
+
+	log.Info("gcp asset collector run started")
+	defer log.Info("gcp asset collector run stopped")
+
+	ticker := time.NewTicker(s.Config.Period)
+	select {
+	case <-ctx.Done():
+		return nil
+	default:
+		s.collectAll(ctx, publisher)
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-ticker.C:
+			s.collectAll(ctx, publisher)
+		}
+	}
+}
+
+func (s *assetsGCP) collectAll(ctx context.Context, publisher stateless.Publisher) error {
+	return nil
+}
