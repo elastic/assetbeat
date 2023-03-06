defe84d364ed5c5039f8e63ee4f8d88a739d88a6
tommyers-elastic <106530686+tommyers-elastic@users.noreply.github.com>
2023-03-06 10:49:20 +0000

    integration tests -> e2e tests

--- tests/e2e/assets_aws_test.go
+++ tests/e2e/assets_aws_test.go
@@ -15,9 +15,9 @@
 // specific language governing permissions and limitations
 // under the License.
 
-//go:build integration
+//go:build e2e
 
-package integration
+package e2e
 
 import (
 	"context"
