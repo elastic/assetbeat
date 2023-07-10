bd0f34f4902043b1bcb721624586181716251fb4
girodav <1390902+girodav@users.noreply.github.com>
2023-07-10 13:54:06 +0100

    Test git status

--- .buildkite/scripts/package.sh
+++ .buildkite/scripts/package.sh
@@ -10,19 +10,18 @@ if [ "$WORKFLOW" = "snapshot" ] ; then
     export SNAPSHOT="true"
 fi
 
-# Install prerequirements (go, mage...)
-MY_DIR=$(dirname $(readlink -f "$0"))
-source $MY_DIR/install-prereq.sh
+git status
 
-# Download Go dependencies
-go mod download
 
-# Packaging the assetbeat binary
-# mage package
-
-echo $PWD
-ls -alrt
-
-git --git-dir="$PWD/.git" rev-parse > /dev/null 2>&1
-# Generate the CSV dependency report
-mage dependencyReport
+## Install prerequirements (go, mage...)
+#MY_DIR=$(dirname $(readlink -f "$0"))
+#source $MY_DIR/install-prereq.sh
+#
+## Download Go dependencies
+#go mod download
+#
+## Packaging the assetbeat binary
+#mage package
+#
+## Generate the CSV dependency report
+#mage dependencyReport
