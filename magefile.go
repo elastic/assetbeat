e33b290b7a2970be20779691cdb323c1da36e0f1
girodav <1390902+girodav@users.noreply.github.com>
2023-07-11 11:28:51 +0100

    Change deps order in mage check

--- magefile.go
+++ magefile.go
@@ -79,11 +79,11 @@ func DependencyReport() error {
 func Check() {
 	// these are not allowed in parallel
 	mg.SerialDeps(
+		mage.Deps.CheckModuleTidy,
 		Lint,
 		Format,
 		CheckLicenseHeaders,
 		Notice,
-		mage.Deps.CheckModuleTidy,
 		mage.CheckNoChanges,
 	)
 }
