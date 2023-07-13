478bd02634ae70717a0c2da7b95989bd8d6657ec
girodav <1390902+girodav@users.noreply.github.com>
2023-07-13 15:56:31 +0100

    Uncomment release-manager step

--- .buildkite/scripts/publish.sh
+++ .buildkite/scripts/publish.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 WORKFLOW=$1
 # The "branch" here selects which "$BRANCH.gradle" file of release manager is used
-export VERSION=$(grep DefaultVersion version/version.go | cut -f2 -d "\"" | tail -n1)
+export VERSION=$(grep defaultVersion version/version.go | cut -f2 -d "\"" | tail -n1)
 MAJOR=$(echo $VERSION | awk -F. '{ print $1 }')
 MINOR=$(echo $VERSION | awk -F. '{ print $2 }')
 if [ -n "$(git ls-remote --heads origin $MAJOR.$MINOR)" ] ; then
@@ -24,24 +24,24 @@ chmod a+r build/distributions/*
 # Allow other users write access to create checksum files
 chmod a+w build/distributions
 
-## Shared secret path containing the dra creds for project teams
+# Shared secret path containing the dra creds for project teams
 echo "Retrieving DRA crededentials..."
 DRA_CREDS=$(vault kv get -field=data -format=json kv/ci-shared/release/dra-role)
 
-# Run release-manager
-#echo "Running release-manager container..."
-#IMAGE="docker.elastic.co/infra/release-manager:latest"
-#docker run --rm \
-#  --name release-manager \
-#  -e VAULT_ADDR=$(echo $DRA_CREDS | jq -r '.vault_addr') \
-#  -e VAULT_ROLE_ID=$(echo $DRA_CREDS | jq -r '.role_id') \
-#  -e VAULT_SECRET_ID=$(echo $DRA_CREDS | jq -r '.secret_id') \
-#  --mount type=bind,readonly=false,src="${PWD}",target=/artifacts \
-#  "$IMAGE" \
-#    cli collect \
-#      --project assetbeat \
-#      --branch "${BRANCH}" \
-#      --commit "${BUILDKITE_COMMIT}" \
-#      --workflow "${WORKFLOW}" \
-#      --version "${VERSION}" \
-#      --artifact-set main \
+ Run release-manager
+echo "Running release-manager container..."
+IMAGE="docker.elastic.co/infra/release-manager:latest"
+docker run --rm \
+  --name release-manager \
+  -e VAULT_ADDR=$(echo $DRA_CREDS | jq -r '.vault_addr') \
+  -e VAULT_ROLE_ID=$(echo $DRA_CREDS | jq -r '.role_id') \
+  -e VAULT_SECRET_ID=$(echo $DRA_CREDS | jq -r '.secret_id') \
+  --mount type=bind,readonly=false,src="${PWD}",target=/artifacts \
+  "$IMAGE" \
+    cli collect \
+      --project assetbeat \
+      --branch "${BRANCH}" \
+      --commit "${BUILDKITE_COMMIT}" \
+      --workflow "${WORKFLOW}" \
+      --version "${VERSION}" \
+      --artifact-set main \
